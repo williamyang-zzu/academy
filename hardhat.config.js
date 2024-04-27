178c72beda14418eefad8610c02c5fcc0939ec6f
williamyang-zzu <1654263161@qq.com>
2024-04-28 00:41:04 +0800

    sepolia scan contracts verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+
+// cross the great wall,if you are using a vpn, 
+// https://github.com/smartcontractkit/full-blockchain-solidity-course-js/discussions/2247#discussioncomment-5496669
+const { ProxyAgent, setGlobalDispatcher } = require("undici");
+const proxyAgent = new ProxyAgent("http://127.0.0.1:9999");
+setGlobalDispatcher(proxyAgent);
 
 // Ensure your configuration variables are set before executing the script
 const { vars } = require("hardhat/config");
@@ -6,6 +13,7 @@ const { vars } = require("hardhat/config");
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and add it to the configuration variables
 const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+//npx hardhat verify --network sepolia 0x9af3a54bf636e20985a05d0af0c31622c34d6860 "bitlayertothemoon"
 
 // Add your Sepolia account private key to the configuration variables
 // To export your private key from Coinbase Wallet, go to
@@ -15,8 +23,11 @@ const INFURA_API_KEY = vars.get("INFURA_API_KEY");
 // Beware: NEVER put real Ether into testing accounts
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
+const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
+
 console.log('INFURA_API_KEY', INFURA_API_KEY);
 console.log('SEPOLIA_PRIVATE_KEY', SEPOLIA_PRIVATE_KEY);
+console.log('ETHERSCAN_API_KEY', ETHERSCAN_API_KEY);
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -27,4 +38,13 @@ module.exports = {
       accounts: [SEPOLIA_PRIVATE_KEY],
     },
   },
+  // ...rest of the config...
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  }
 };
